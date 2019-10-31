0a86a11f2cc16739520be52e74ddb27659d85d9e
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-10-31 17:21:54 +0530

    form-sanitization

--- register.php
+++ register.php
@@ -1,4 +1,51 @@
-<!DOCTYPE html>
+<?php
+// Form Sanitization
+
+// username sanitization function
+function sanitizeFormUsername($inputText)
+{
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    return $inputText;
+}
+
+// string sanitization function
+function sanitizeFormString($inputText)
+{
+    $inputText = strip_tags($inputText);
+    $inputText = strip_replace(" ", "", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+
+// password sanitization function
+function sanitizeFormPassword($inputText)
+{
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
+if (isset($_POST['loginButton'])) {
+    //Login Button Was Pressed
+
+}
+
+if (isset($_POST['registerButton'])) {
+    //Register Button Was Pressed
+
+    // Register Form Sanitization 
+    $username = sanitizeFormUsername($_POST['username']);
+    $firstname = sanitizeFormString($_POST['firstname']);
+    $lastname = sanitizeFormString($_POST['lastname']);
+    $email = sanitizeFormString($_POST['email']);
+    $email2 = sanitizeFormString($_POST['email2']);
+    $password = sanitizeFormPassword($_POST['password']);
+    $password2 = sanitizeFormPassword($_POST['password2']);
+}
+
+
+?>
+
 <html lang="en">
 
 <head>
@@ -22,6 +69,47 @@
                 <input id="loginPassword" name="loginPassword" type="password" placeholder="Enter Password" required>
             </p>
             <button type="submit" name="loginButton">LOG IN</button>
+
+
+
+            <form id="registerForm" action="register.php" method="POST">
+                <h2>Create your free Account</h2>
+                <p>
+                    <label for="username">Username: </label>
+                    <input id="username" name="username" type="text" placeholder="Enter User Name" required>
+                </p>
+
+                <p>
+                    <label for="firstname">First Name: </label>
+                    <input id="firstname" name="firstname" type="text" placeholder="Enter your First Name" required>
+                </p>
+                <p>
+                    <label for="lastname">Last Name: </label>
+                    <input id="lastname" name="lastname" type="text" placeholder="Enter your Last Name" required>
+                </p>
+
+                <p>
+                    <label for="email">Email: </label>
+                    <input id="email" name="email" type="email" placeholder="Enter your Email" required>
+                </p>
+
+                <p>
+                    <label for="email2">Confirm Email: </label>
+                    <input id="email2" name="email2" type="email" placeholder="Re-Enter your Email" required>
+                </p>
+
+                <p>
+                    <label for="password">Password: </label>
+                    <input id="password" name="password" type="password" placeholder="Enter Password" required>
+                </p>
+
+                <p>
+                    <label for="password2">Confirm Password: </label>
+                    <input id="password2" name="password2" type="password" placeholder="Re-Enter Password" required>
+                </p>
+
+                <button type="submit" name="registerButton">SIGN UP</button>
+
 
     </div>
 
