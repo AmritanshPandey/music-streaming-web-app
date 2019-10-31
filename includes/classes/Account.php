e65635f51adaf20e19d7d139f83ada76805b70bd
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-10-31 18:32:40 +0530

    firstname-and-lastname-validation-added

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -3,7 +3,7 @@ class Account
 {
     private $errorArray;
     public function __construct()
-    { 
+    {
         $this->errorArray = array();
     }
     public function register($un, $fn, $ln, $em, $em2, $pw, $pw2)
@@ -19,21 +19,31 @@ class Account
     // Validating user name
     private function validateUsername($un)
     {
-        if(strlen($un) > 25 || strlen($un) < 5){
-            array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+        if (strlen($un) > 25 || strlen($un) < 5) {
+            array_push($this->errorArray, "Your Username must be between 5 and 25 characters");
             return;
         }
 
         //TODO: check if username exists
-     }
+    }
 
     // Validating first name
     private function validateFirstname($fn)
-    { }
+    {
+        if (strlen($fn) > 50 || strlen($fn) < 2) {
+            array_push($this->errorArray, "Your First Name must be between 2 and 50 characters");
+            return;
+        }
+    }
 
     // Validating last name
     private function validateLastname($ln)
-    { }
+    {
+        if (strlen($ln) > 50 || strlen($ln) < 2) {
+            array_push($this->errorArray, "Your Last Name must be between 2 and 50 characters");
+            return;
+        }
+    }
 
     // Validating emails
     private function validateEmails($em, $em2)
